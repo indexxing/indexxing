4b5e810b38d6ad263d01d689bebf653c9a4ef38d
Index <xv.media.address@gmail.com>
2024-03-06 13:58:57 -0600

    Update GUI Project

--- projects/gui/script.js
+++ projects/gui/script.js
@@ -1,36 +1,56 @@
+const Workspace = document.getElementById('workspace')
+const ObjectTemplates = document.getElementById('object-templates')
+
+let SelectedObject = null;
+
+let Dragging = false
+let CanDrag = true
+let Resizing = false
+
+const PropertiesObject = {
+    UILabel: [
+        {
+            name: "Text",
+            onUpdate: function(element, newValue){ element.getElementsByClassName('text')[0].value = newValue }
+        }
+    ]
+}
+
 document.addEventListener('DOMContentLoaded', function(){
-    Array.from(document.getElementsByClassName('draggable')).forEach(element => {InitializeElement(element)})
+    Array.from(document.getElementsByClassName('object')).forEach(element => {
+        if (element.parentElement.id !== "object-templates") {
+            InitializeElement(element)
+            element.setAttribute('data-initialized', true)
+        }
+    })
 });
 
 function InitializeElement(element) {
+    UpdateWorkspaceProperties()
     console.log('Started InitializeElement Function for ', element)
+
     element.style.position = 'absolute'
+
+    element.addEventListener('click', function(){
+        SelectObject(element)
+    })
 
     // Drag Button
     const DragButton = element.getElementsByClassName('drag-button')[0]
-    let Dragging = false
-    DragButton.addEventListener('click', function(){ DisableObjectOptions(element); Dragging = true });
-
-    // Move Object when Dragging is Enabled
-    document.addEventListener('mousemove', function(e){
-        if (Dragging === true) {
-            e.preventDefault()
-
-            element.style.top = (e.clientY-(element.offsetHeight/2))+'px';
-            element.style.left = (e.clientX-(element.offsetWidth/2)) + 'px';
+    DragButton.addEventListener('mousedown', function(){
+        SelectObject(element)
+        if (CanDrag === true) {
+            DisableObjectOptions(element);
+            Dragging = true
         }
     });
 
     // Resize
     const ResizeButton = element.getElementsByClassName('resize-button')[0]
-    let Resizing = false
-    let ShowAlert = true
     ResizeButton.addEventListener('click', function(){
+        SelectObject(element)
         DisableObjectOptions(element)
         Resizing = true
-        if (ShowAlert === true) {
-            ShowAlert = false
-        }
         element.style.resize = 'both'
         element.style.overflow = 'auto'
     });
@@ -40,18 +60,48 @@ function InitializeElement(element) {
     DeleteButton.addEventListener('click', function(){
         element.remove()
         console.log('Deleted element')
+        UpdateWorkspaceProperties()
     });
-    
-    // Finish
-    document.addEventListener('keyup', function(e){
+}
+
+// Move Object when Dragging is Enabled
+document.addEventListener('mousemove', function(e){
+    if (Dragging === true) {
         e.preventDefault()
-        if (e.key === "Enter" && (Dragging === true || Resizing === true)) {
-            document.body.focus()
-            Dragging = false
-            Resizing = false
-            Finish(element)
-        }
-    })
+
+        SelectedObject.style.top = (e.clientY-(SelectedObject.offsetHeight/2))+'px';
+        SelectedObject.style.left = (e.clientX-(SelectedObject.offsetWidth/2)) + 'px';
+    }
+});
+
+// Finish
+document.addEventListener('mouseup', function(){
+    Dragging = false
+    CanDrag = true
+    Resizing = false
+    Finish(SelectedObject)
+});
+
+function NewObject(className) {
+    const ClassTemplate = ObjectTemplates.querySelector(`[data-class="${className}"]`)
+    if (ClassTemplate !== undefined) {
+        const Clone = ClassTemplate.cloneNode(true)
+        Workspace.appendChild(Clone)
+        InitializeElement(Clone)
+        Clone.setAttribute('data-initialized', true)
+    } else {
+        alert('That class does not exist!')
+    }
+}
+
+function SelectObject(element) {
+    if (SelectedObject !== null) {
+        SelectedObject.classList.remove('selected')
+    }
+    if (SelectedObject !== element) {
+        SelectedObject = element
+        SelectedObject.classList.add('selected')
+    }
 }
 
 function DisableObjectOptions(element) {
@@ -63,13 +113,16 @@ function Finish(element) {
     element.removeEventListener('keyup', Finish)
 
     const Options = Array.from(element.getElementsByClassName('object-options')[0].children)
-
-    const DragButton = element.getElementsByClassName('drag-button')[0]
-    const ResizeButton = element.getElementsByClassName('resize-button')[0]
-    const DeleteButton = element.getElementsByClassName('delete-button')[0]
+    Options.forEach(option => {option.disabled = false})
 
     element.style.resize = 'none'
     element.style.overflow = ''
+}
 
-    Options.forEach(option => {option.disabled = false})
+function UpdateWorkspaceProperties() {
+    const WorkspaceProperties = document.getElementById('workspace-properties')
+
+    WorkspaceProperties.innerHTML = `
+    <li>Object Count: ${Workspace.children.length}</li>
+    `
 }
